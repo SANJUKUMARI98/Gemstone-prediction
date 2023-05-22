476a97da18f6b3fad6b7e680583d74ed66865843
SANJUKUMARI98 <sanju080598@gmail.com>
2023-05-22 15:08:22 +0530

    project structure

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import find_packages,setup
 
 from typing import List
 
+HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
+        if HYPEN_E_DOT  in requirements:
+            requirements.remove(HYPEN_E_DOT)
+         
+
         return requirements
 
 
@@ -15,6 +20,6 @@ setup(
     version='0.0.1',
     author='sanju',
     author_mail ='sanju080598@gmail.com',
-    install_requies=get_requirements('requirements.txt'),
+    install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
 )
